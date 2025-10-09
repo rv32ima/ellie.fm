31e66bd8e519c43b1d5e20aef03f307c58503f6a
ellie <me@ellie.fm>
2025-10-09 19:20:13 +0000

    various tweaks and changes

--- src/app.tsx
+++ src/app.tsx
@@ -60,8 +60,8 @@ export function App() {
 
 
   return (
-    <main className="flex flex-col w-full h-full xl:p-20">
-      <div className="main-body flex flex-col bg-yellow-100 text-black p-5 text-center border-4 border-red-500 border-double">
+    <main className="flex flex-col w-full h-full items-center align-center justify-center p-8 xl:p-20">
+      <div className="max-w-4xl main-body flex flex-col bg-yellow-100 text-black p-5 text-center border-4 border-red-500 border-double">
         <audio src="/wrenchHit.wav" ref={clickSound} preload={"auto"} />
         <audio src="/playerConnect.wav" autoplay preload={"auto"} />
         <audio src="/Drums.ogg" autoplay loop preload={"auto"} />
@@ -70,11 +70,8 @@ export function App() {
           hi! welcome to The Ellie Zone!
           <img className="px-2" src="/star.png" />
         </h1>
-        <span>i am ellie! i am a 22 year old half-japanese ex-mormon trans girl living in Seattle, WA!</span>
-        <span>i currently work at <ClickLink href="https://tvscientific.com">tvScientific</ClickLink> leading their ML platform development.</span>
-        <span>at tvscientific, i work with some awesome trans/neurodivergent/open-minded people on the data science team.</span>
-        <span>if that interests you, and you'd like a referral, email me at me (at) this domain :-)</span>
-        <span>enough work talk. this is my little corner of the internet ^_^ please enjoy!!</span>
+        <span>i am ellie! i am a 22 year old half-japanese girl residing in seattle</span>
+        <span>this is my little corner of the internet ^_^ please enjoy!!</span>
         <div className="flex flex-col space-y-4 pt-4">
           <div className="flex outline-2 outline-green-500 p-2 pb-4">
             <div className="flex flex-col w-full items-center justify-center align-center text-center">
@@ -83,7 +80,6 @@ export function App() {
               my other internet profiles!
               <img className="px-2" src="/star.png" />
               </h1>
-              <SocialLink type="bluesky" href="https://bsky.app/profile/did:plc:wovqqe4chzpghn7hre46nvku" text="ellie.fm" />
               <SocialLink type="last.fm" href="https://www.last.fm/user/ellieidb" />
               <SocialLink type="github" href="https://github.com/rv32ima" />
               <SocialLink type="spotify" href="https://open.spotify.com/user/metarules" />
@@ -92,28 +88,27 @@ export function App() {
           <div className="flex flex-col py-2 outline-2 outline-blue-500">
             <h2>important things that you should maybe perhaps read! (no pressure)</h2>
             <ClickLink href="/pay_transparency.txt">like... my pay transparency document!</ClickLink>
-            <ClickLink href="https://tr4nny.net">or my blog! (still a work in progress)</ClickLink>
             <ClickLink href="https://diskinserted.llc">or check out my consultancy!</ClickLink>
           </div>
         </div>
-        <div className="flex justify-center pt-4 space-y-1 align-center items-center">
-          <BigAnimatedBadge badge="acab" />
-          <BigAnimatedBadge badge="anarchynow" />
+        <div className="flex flex-wrap justify-center pt-8 align-center items-center">
+          <AnimatedBadge badge="fl_jap" />
+          <AnimatedBadge badge="fl_usa2" />
           <AnimatedBadge badge="anarchist_copy" />
           <AnimatedBadge badge="best_viewed" />
           <AnimatedBadge badge="eff2" />
           <AnimatedBadge badge="gmail_copy1" />
-          <AnimatedBadge badge="this_website_gay" />
+          <Badge badge="Hhacker" />
+          <Badge badge="punk_software" />
           <AnimatedBadge badge="mac_micro" />
         </div>
-        <div className="flex justify-center pt-4 space-y-1 align-center items-center">
-          <AnimatedBadge badge="macosmade" />
-          <AnimatedBadge badge="fl_jap" />
-          <AnimatedBadge badge="fl_usa2" />
-          <AnimatedBadge badge="defund_badge" />
-          <Badge badge="punk_software" />
-          <AnimatedBadge badge="webpassion" />
-          <Badge badge="Hhacker" />
+        <div className="flex flex-wrap justify-center pt-4 align-center items-center">
+          <BigAnimatedBadge badge="acab" />
+          <BigAnimatedBadge badge="anarchynow" />
+          <BigAnimatedBadge badge="macosmade" />
+          <BigAnimatedBadge badge="this_website_gay" />
+          <BigAnimatedBadge badge="defund_badge" />
+          <BigAnimatedBadge badge="webpassion" />
         </div>
       </div>
     </main>
